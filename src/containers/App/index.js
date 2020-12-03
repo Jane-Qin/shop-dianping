552def9343e460ebcb365bda67ccbcca9eb38cf4
qying <1479795154@qq.com>
2020-12-03 21:59:40 +0800

    通过错误封装

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,11 +1,30 @@
 import './style.css';
+import React, { Component } from 'react';
+import ErrorToast from '../../components/ErrorToast'
+import { actions as appActions,getError } from '../../redux/modules/app';
+import { bindActionCreators } from 'redux';
+import {connect} from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    const {error,appActions:{clearError}}=this.props;
+    return (
+      <div className="App">
+        {error?<ErrorToast msg={error} clearError={clearError}/>:null}
+      </div>
+    );
+  }
 }
+const mapStateToProps=(state,props)=>{
+  return {
+    error:getError(state)
+  }
+}
+const mapDispatchToProps=(dispatch)=>{
+  return {
+    appActions:bindActionCreators(appActions,dispatch)
+  }
 
-export default App;
+} 
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/redux/modules/app.js
+++ src/redux/modules/app.js
@@ -1,4 +1,25 @@
+const types={
+    CLEAR_ERROR:"APP/CLEAR_ERROR"
+}
+//action creators
+export const actions={
+    clearError:()=>({
+        type:types.CLEAR_ERROR
+    })
+}
+
 const reducer=(state={},action)=>{
+    const {type,error}=action;
+    if(type===types.CLEAR_ERROR){
+        return {...state,error:null}
+    }else if(error){
+        return {...state,error}
+    }
     return state;
 }
 export default reducer;
+
+//selectors
+export const getError=(state)=>{
+    return state.app.error
+}
